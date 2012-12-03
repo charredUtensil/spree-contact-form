7f44860ac84c6a89b3f79909cbdf8476916ae317
Kirill Zonov <graffzon@gmail.com>
2012-12-03 18:34:09 +0400

    fixed indentation and added attr_accessible for topic fields

--- app/models/spree/contact_topic.rb
+++ app/models/spree/contact_topic.rb
@@ -1,6 +1,8 @@
 module Spree
-	class ContactTopic < ActiveRecord::Base
-	  validates :name, :emails, :presence => true
-	  validates :name,          :uniqueness => true
-	end
+  class ContactTopic < ActiveRecord::Base
+    attr_accessible :name, :emails
+
+    validates :name, :emails, :presence => true
+    validates :name,          :uniqueness => true
+  end
 end
