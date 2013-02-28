a5a1d3c956b58e94c2d1e8f98925504416427d7e
Ag <github@assembledgoods.com>
2013-02-28 13:52:01 -0600

    add to admin configurations menu

--- app/overrides/admin.rb
+++ app/overrides/admin.rb
@@ -1,12 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
-                     :name => "converted_admin_configurations_menu_394888068",
-                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                     # :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= configurations_menu_item(I18n.t('contact_topics'), admin_contact_topics_path, I18n.t('contact_topics_description')) %>",
-                     :disabled => false)
-
-# Deface::Override.new(:virtual_path => "spree/layouts/admin",
-#                      :name => "promo_admin_tabs",
-#                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-#                      :text => "<%= tab(:promotions, :url => spree.admin_promotions_path) %>",
-#                      :disabled => false)
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
+                    :name => "converted_admin_configurations_menu_394888068",
+                    :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+                    :text => %q{<%= configurations_sidebar_menu_item I18n.t('contact_topics'), admin_contact_topics_path %>},
+                    :disabled => false)
