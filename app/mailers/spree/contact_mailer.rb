6d9243e3f08a10cd057718fad4d98d9fb075b597
Ag <github@assembledgoods.com>
2013-05-02 10:45:52 -0500

    update ContactMailer to use Spree::BaseMailer and include from_address

--- app/mailers/spree/contact_mailer.rb
+++ app/mailers/spree/contact_mailer.rb
@@ -1,12 +1,12 @@
 module Spree
-	class ContactMailer < ActionMailer::Base
-	  helper "spree/base"
+  class ContactMailer < BaseMailer
+    helper 'spree/base'
 
-	  def message_email(message)
-	    subject = "#{Spree::Config[:site_name]} - Message from #{message.email}"
+    def message_email(message)
+      subject = "#{Spree::Config[:site_name]} - Message from #{message.email}"
 
-	    @message = message
-	    mail(:to => message.topic.emails, :subject => subject, :reply_to => message.email) 
-	  end
-	end	
+      @message = message
+      mail(:to => message.topic.emails, :from => from_address, :subject => subject, :reply_to => message.email)
+    end
+  end	
 end
