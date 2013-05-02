65cc7f6ca22050d38dd8194aa8350cec25679c0d
Ag <github@assembledgoods.com>
2013-05-02 10:55:41 -0500

    don't validate order_number format

--- app/models/spree/message.rb
+++ app/models/spree/message.rb
@@ -5,7 +5,7 @@ module Spree
     validates :name, :message, :topic_id, :presence => true
 
     validates :email,        :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-    validates :order_number, :format => /(^$)|(^R\d{9}$)/i
+    # validates :order_number, :format => /(^$)|(^R\d{9}$)/i
 
     belongs_to :topic, :class_name => 'ContactTopic'
   end
