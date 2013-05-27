0f02b909d1b303f19d35cc35540df1c603843a76
Ag <github@assembledgoods.com>
2013-05-27 07:56:05 -0500

    Add a filter to control spam coming through contact us form

--- app/controllers/spree/contact_controller.rb
+++ app/controllers/spree/contact_controller.rb
@@ -1,25 +1,28 @@
 module Spree
 	class ContactController < BaseController
-	  before_filter :load_topics
+    before_filter :load_topics
 
-	  def show
-	    @message = Message.new
-	  end
+    def show
+      @message = Message.new
+    end
 
-	  def create
-	    @message = Message.new(params[:message] || {})
-	    if @message.save
-	      ContactMailer.message_email(@message).deliver
-	      flash[:notice] = t('contact_thank_you')
-	      redirect_to root_path
-	    else
-	      render :action => 'show'
-	    end
-	  end
+    def create
+      @message = Message.new(params[:message] || {}) unless spam?
+      if @message.save
+        ContactMailer.message_email(@message).deliver unless spam?
+        flash[:notice] = t('contact_thank_you')
+        redirect_to root_path
+      else
+        render :action => 'show'
+      end
+    end
 
 	private
-	  def load_topics
-	    @topics = ContactTopic.all
-	  end
+    def load_topics
+      @topics = ContactTopic.all
+    end
+    
+    def spam?
+      params[:message][:filter].present? || params[:message][:filter].nil?
 	end	
 end
